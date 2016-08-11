4bfde7f6b1f9edd407ddd39e8b73ec93e5109149
Qin An <anqin314@gmail.com>
2016-08-11 14:02:46 -0700

    access files finished

--- cnv_access.sh
+++ cnv_access.sh
@@ -2,6 +2,8 @@
 PYTHON="/share/apps/python2.7.6/bin/python"
 cnvkit_dir="/home/qan/cnvkit/"
 
-$PYTHON ${cnvkit_dir}cnvkit.py  target ./cnvkit_annot/access-5kb-mappable.hg19.bed --split --annotate ./cnvkit_annot/refFlat.txt -o ./cnvkit_annot/Targets_hg19.bed 
+$PYTHON ${cnvkit_dir}cnvkit.py access /labshares/fanlab/anqin/REF/Hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa -x ./cnvkit_annot/wgEncodeDukeMapabilityRegionsExcludable.bed -o ./cnvkit_annot/access-5k-mappable.hg19.bed  
 
-$PYTHON ${cnvkit_dir}cnvkit.py antitarget ./cnvkit_annot/access-5kb-mappable.hg19.bed -g ./cnvkit_annot/access-5kb-mappable.hg19.bed -o ./cnvkit_annot/Background_hg19.bed
+$PYTHON ${cnvkit_dir}cnvkit.py  target ./cnvkit_annot/access-5k-mappable.hg19.bed  --split  -o ./cnvkit_annot/Targets_hg19.bed --annotate ./cnvkit_annot/refFlat_hg19.txt 
+
+$PYTHON ${cnvkit_dir}cnvkit.py antitarget ./cnvkit_annot/access-5k-mappable.hg19.bed -g ./cnvkit_annot/access-5k-mappable.hg19.bed -o ./cnvkit_annot/Background_hg19.bed
---
+++ cnv_for_each_sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PYTHON="/share/apps/python2.7.6/bin/python"
+cnvkit_dir="/home/qan/cnvkit/"
+
+# ${1} to be the bam file
+
+cnvkit.py coverage Sample.bam my_targets.bed -o Sample.targetcoverage.cnn
+cnvkit.py coverage Sample.bam my_antitargets.bed -o Sample.antitargetcoverage.cnn
+
+$PYTHON ${cnvkit_dir}cnvkit.py coverage  ${1} /labshares/fanlab/anqin/cfDNA_project/Genome_wide_CNV/cnvkit_annot/Targets_hg19.bed -o ${1}.hg19.targetcoverage.cnn
+
+$PYTHON ${cnvkit_dir}cnvkit.py coverage  ${1} /labshares/fanlab/anqin/cfDNA_project/Genome_wide_CNV/cnvkit_annot/Background_hg19.bed -o ${1}.hg19.antitargetcoverage.cnn
